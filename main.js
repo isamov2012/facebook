6d0a9d9aa96abade7c30079bf06476abf6e1c115
Isam <isamov2012@hotmail.com>
2021-01-17 13:48:09 +0100

    Title change
    title changed from facebook to fakebook

--- main.js
+++ main.js
@@ -1,44 +1,48 @@
-const database=[
-    {
-        username:'Isam',
-        password:'12'
-    },
-    {
-        username:'Tanya',
-        password:'123'
-    },
-    {
-        username:'Melika',
-        password:'1234'
-    },
-]
-const newsfeed=[
-    {
-        username:'Melika',
-        timeline:'We were at the pool today!'
-    },
-    {
-        username:'Adam',
-        timeline:'I poobed many times today!'
-    },
-    {
-        username:'Tanya',
-        timeline:'I burned the food today '
+const database = [
+  {
+    username: "Isam",
+    password: "12",
+  },
+  {
+    username: "Tanya",
+    password: "123",
+  },
+  {
+    username: "Melika",
+    password: "1234",
+  },
+];
+const newsfeed = [
+  {
+    username: "Melika",
+    timeline: "We were at the pool today!",
+  },
+  {
+    username: "Adam",
+    timeline: "I poobed many times today!",
+  },
+  {
+    username: "Tanya",
+    timeline: "I burned the food today ",
+  },
+];
+const usernamePrompt = prompt("Your username?");
+const passwordPrompt = prompt("Your password?");
+function isUsernameValid(user, pass) {
+  for (let i = 0; i < database.length; i++) {
+    if (user === database[i].username && pass === database[i].password) {
+      return true;
     }
-]
-const usernamePrompt=prompt('Your username?');
-const passwordPrompt=prompt('Your password?');
-function isUsernameValid(user,pass){
-    for(let i=0;i<database.length;i++){
-        if (user===database[i].username&&pass===database[i].password){
-            return true
-        }
-    }
-    return false;
+  }
+  return false;
 }
-function signIn(user,pass){
-    isUsernameValid(user,pass)?console.log(newsfeed):alert('wrong username and password!')
+function signIn(user, pass) {
+  isUsernameValid(user, pass)
+    ? console.log(newsfeed)
+    : alert("wrong username and password!");
 }
-signIn(usernamePrompt,passwordPrompt)
-document.getElementsByClassName('listElement')[Math.floor(Math.random()*2.5)].style.backgroundColor='red'
-document.querySelector('li').getAttribute('listElement')
+signIn(usernamePrompt, passwordPrompt);
+document.getElementsByClassName("listElement")[
+  Math.floor(Math.random() * 2.5)
+].style.backgroundColor = "red";
+document.querySelector("li").getAttribute("listElement");
